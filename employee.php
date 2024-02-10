9c4c94659eae11f60c124856bcb85e89c5bbaaf9
Sergey Kornyushin <nerub3@rambler.ru>
2024-02-10 17:49:49 +0300

    Готово задание 2.1

--- employee.php
+++ employee.php
@@ -7,7 +7,11 @@ class Employee{               //создали класс
 
     public function __construct($id, $name, $sal, $date){        //конструктор класса
 
-        $this->validateId($id);                                  //метод валидации
+        $this->validateId($id);                                  
+        $this->validateName($name);  
+        $this->validateSal($sal); 
+        $this->validateDate($date); 
+
 
         $this->id = $id;
         $this->name = $name;
@@ -21,6 +25,21 @@ class Employee{               //создали класс
     public function validateId($id){
         if ($id <= 0 || !is_int($id)) {
             throw new InvalidArgumentException("Неправильно введен ID, ID должен быть целым числом от 1 и выше.");
+        }
+    }
+    public function validateName($name){
+        if (strlen($name) < 2 || strlen($name) >30) {
+            throw new InvalidArgumentException("Неправильно введено Имя, Имя должно быть словом от 2 до 30 символов.");
+        }
+    }
+    public function validateSal($sal){
+        if ($sal < 0  || is_string($sal) ) {
+            throw new InvalidArgumentException("Неправильно введена Зарплата, Зарплата должна быть неотрицательным числом.");
+        }
+    }
+    public function validateDate($date){
+        if (empty($date)) {
+            throw new InvalidArgumentException("Дата не заполнена.");
         }
     }
 }
